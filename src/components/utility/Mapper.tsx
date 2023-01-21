0996873d62f28f73348ff429bd080357fbb59394
samboosa5k <jasper.verbon@gmail.com>
2023-01-21 03:38:55 +0100

    [component] Reusable component mapper to generate dynamic lists

--- src/components/utility/Mapper.tsx
+++ src/components/utility/Mapper.tsx
@@ -1,13 +1,26 @@
-import { FC, useId, useMemo } from 'react';
-import { GenericDataObject, GenericGuard } from '../../@types';
+import { useId, useMemo } from 'react';
+import { GenericDataObject, GenericGuard, GenericValue } from '../../@types';
 
+/**
+ * MapperProps --> Interface to deal with the reusable <Mapper>
+ * component. TS will show errors when an item in the array of data does not
+ * match the structure of the "model" required by the component-prop.
+ */
 interface MapperProps {
-    model: GenericDataObject;
+    model: GenericDataObject<GenericValue>;
     data: Array<MapperProps['model']>;
     Comp: <T extends MapperProps['model']>(props: T) => JSX.Element;
 }
 
-export const Mapper: FC<MapperProps> = ({ Comp, model, data }: MapperProps) => {
+/**
+ * Mapper - The one-size-fits "most" list generator:
+ * When React & Typescript work together to make magic happen
+ * @param Comp
+ * @param model
+ * @param data
+ * @constructor
+ */
+export const Mapper = ({ Comp, model, data }: MapperProps) => {
     const uniq = useId();
     const isProperData = useMemo(() => {
         return GenericGuard<typeof model>(data[0]);
@@ -19,7 +32,7 @@ export const Mapper: FC<MapperProps> = ({ Comp, model, data }: MapperProps) => {
         return (
             <>
                 {data &&
-                    data.map((item: typeof model, idx: number) => (
+                    data.map((item: MapperProps['model'], idx: number) => (
                         <Comp key={`mapped_${idx}_${uniq}`} {...item} />
                     ))}
             </>
