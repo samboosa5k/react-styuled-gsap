50e84ada73f81009030e7cadab1e33ffa12b53ce
samboosa5k <jasper.verbon@gmail.com>
2023-03-14 00:25:26 +0100

    [temp] App.tsx commented code for Animator HOC setup

--- src/pages/StyledPage.tsx
+++ src/pages/StyledPage.tsx
@@ -1,15 +1,15 @@
-import { Animator, slideInOutAnimation } from '@/HOC';
+import {
+    AnimatorBroProvider, // slideInOutAnimation
+} from '@/HOC';
 
 import { List, listDataArray } from '@/modules';
 
 export const StyledPage = () => {
     return (
-        <>
-            <Animator
-                targetClassName="abc"
-                animationCallback={slideInOutAnimation}>
-                <List dataArray={listDataArray.filter((_, idx) => idx > 5)} />{' '}
-            </Animator>
-        </>
+        <AnimatorBroProvider
+        // animationCallback={slideInOutAnimation}
+        >
+            <List dataArray={listDataArray.filter((_, idx) => idx > 5)} />{' '}
+        </AnimatorBroProvider>
     );
 };
